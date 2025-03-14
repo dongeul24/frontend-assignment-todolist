ea3c4fc5e85d37c1ca1797b61d23d195f6713e85
dongeul24 <gogo0232@naver.com>
2025-03-14 11:43:00 +0900

    feat: 투두리스트 불러오기(read) 기능 + 페이지네이션 기능 구현 완료

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-orange-300 text-white py-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <Link href="/">
+          <Image src="/logo.svg" alt="Logo" width={40} height={40} />
+        </Link>
+      </div>
+    </header>
+  );
+}
